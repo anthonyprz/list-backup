6bd6c347bc89c23a07114ac6f767193d6e5d056b
anthony <thony_prz@outlook.com>
2015-09-26 09:13:28 +0000

    tercer commit

--- php/mostrar.php
+++ php/mostrar.php
@@ -1,101 +1,4 @@
-<!DOCTYPE html>
-	<head>
-		
-		<?php session_start(); ?>
-		<title>Lista</title>
-		<meta charset="utf-8"/>
-		<script src="lib/jquery-1.11.3.min.js"></script>
-		<script src="lib/jquery.validate.min.js"></script>
-
-		<link rel="StyleSheet" href="css/stilo.css" type="text/css">
-		<link rel="icon" type="image/png" href="imagenes/List2.png" />
-		<script type="text/javascript">
-		
-		
-
-$(document).ready(function() {
-	
-	 $("#linea1").click(function(){
-        $("#crearlista").slideToggle("linear");
-       
-         	
-    });
-	 $("#linea2").click(function(){
-        $("#compartirlista").slideToggle("linear");
-        
-    });
-	 $("#linea3").click(function(){
-        $(".narchivadas").slideToggle("linear");
-        //document.getElementById("linea3").innerHTML = "<li>NO ARCHIVADAS</li>";
-        $(".sarchivadas").slideToggle("linear");
-       
-       
-    });
-
-});	
-
-
-
-
-		</script>
-		
-	</head>
-	<body style="margin:0;">
-		
-		<div id="banner">
-			<div id="logout"><a href="index.html">LogOut</a></div>
-			<div class="logo">
-			<img src="imagenes/List.png"/>	
-			</div>
-			<!--<div id="logo"></div>-->
-			<div id="bannerletras">
-				<h1></h1></h1>
-			</div>
-		</div><!--banner-->
-
-		<div id="nav">
-		
-	<ul>	
-		<li id="linea1">CREAR</li>
-		<li id="linea2">COMPARTIR</li>
-		<li  id="linea3">ARCHIVADAS</li>
-	</ul>	
-	<!--<ul>	
-		<li id="linea2">COMPARTIR</li>
-			
-	</ul>
-	<ul>	
-		<li  id="linea3">ARCHIVADAS</li>	
-		</ul>-->
-			
-	</div><!--nav-->
-	<form hidden id="crearlista"  method="post" action="php/crearlista.php">	
-		<h2>Crear una Lista</h2>
-		<input type="text" name="nombrelista" placeholder="Nombre de la Lista"/>
-		<br/>
-		<input type="text" name="task1" placeholder="task1"/>
-		<br/>
-		<input type="text" name="task2" placeholder="task2"/>
-		<br/>
-		<input type="text" name="task3" placeholder="task3"/>
-		<br/>
-		<input type="text" name="task4" placeholder="task4"/>
-		<br/>
-		<input type="text" name="task5" placeholder="task5"/>
-		<br/>
-		<input type="submit"  class='btn' value="Submit"/>
-	</form>
-	
-		<form hidden id="compartirlista" method="post" action="php/compartir.php">	
-		<h2>Compartir</h2>
-		<input type="text" name="nombrelista" placeholder="Nombre de la Lista"/>
-		<br/>
-		<input type="text" name="usuarioc" placeholder="¿Con quien la quieres compartir?"/>
-		<br/>
-		<input type="submit" class='btn' value="Submit"/>
-	</form>
-
-	<?php 
+<?php
 	$user = $_SESSION['Usuario'];
 	$iduser = $_SESSION['id_usuario'];
 	
@@ -179,10 +82,8 @@ $(document).ready(function() {
 			
 		 echo "</div><!-- sarchivadas -->";
 	   }
- 
 	
+	header("Location: ../principal.php");
 	
 	
 	?>
-	</body>
-</html>
